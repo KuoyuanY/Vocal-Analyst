92077dbdda1d5c1bc2397515936ae73e727d2772
Kuoyuan Yang <1kuoyuanyang@gmail.com>
2017-09-16 09:25:12 -0400

    uploading files

--- mysite/mank/urls.py
+++ mysite/mank/urls.py
@@ -2,5 +2,6 @@ from django.conf.urls import url, include
 from . import views
 
 urlpatterns = [
-    url(r'^$', views.index, name='index')
+    url(r'^$', views.index, name='index'),
+    url(r'^$', views.simple_upload, name='simple_upload')
 ]
