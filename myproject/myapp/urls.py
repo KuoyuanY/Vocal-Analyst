78ac2039378eb5ce4abd08549a4e6cc8e3c2f5a0
Matthew Molino <matthewgmolino@gmail.com>
2017-09-16 22:26:16 -0400

    Commiting so can pull

--- myproject/myapp/urls.py
+++ myproject/myapp/urls.py
@@ -1,8 +1,9 @@
 # -*- coding: utf-8 -*-
 from django.conf.urls import url
 from myproject.myapp.views import list
+from myproject.myapp.views import result
 
 urlpatterns = [
-    url(r'^list/$', list, name='list')
-    url(r'^result/$', view.result, name='result')
+    url(r'^list/$', list, name='list'),
+    url(r'^result/$', result, name='result')
 ]
--- myproject/urls.py
+++ myproject/urls.py
@@ -8,5 +8,6 @@ from django.contrib import admin
 urlpatterns = [
     url(r'^admin/', include(admin.site.urls)),
     url(r'^myapp/', include('myproject.myapp.urls')),
-    url(r'^$', RedirectView.as_view(url='/myapp/list/', permanent=True)),
+    url(r'^result$', RedirectView.as_view(url='/myapp/result/', permanent=True)),
+    url(r'^list$', RedirectView.as_view(url='/myapp/list/', permanent=True)),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
