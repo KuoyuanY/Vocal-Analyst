9c234430350850fde7739430f5b55825302ddc9a
Kuoyuan Yang <1kuoyuanyang@gmail.com>
2017-09-16 22:16:21 -0400

    got rid of delete

--- myproject/myapp/urls.py
+++ myproject/myapp/urls.py
@@ -1,8 +1,10 @@
 # -*- coding: utf-8 -*-
 from django.conf.urls import url
 from myproject.myapp.views import list
+from myproject.myapp.views import result
 
 
 urlpatterns = [
-    url(r'^list/$', list, name='list')
+    url(r'^list/$', list, name='list'),
+    url(r'^result/$', result, name='result')
 ]
